4ebc3fa97ff78bb1bdc5e659e2009b9ff3382bed
yann <jeanmaireyann.pro@gmail.com>
2023-03-23 16:22:52 +0100

    fix previous commit

--- src/main/java/front/frontendDashboard/test/src/app/auth.guard.ts
+++ src/main/java/front/frontendDashboard/test/src/app/auth.guard.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import {CanActivate, Router} from '@angular/router';
-import {AuthServiceService} from "./auth-service.service";
 
 @Injectable({
   providedIn: 'root'
--- src/main/java/front/frontendDashboard/test/src/main.ts
+++ src/main/java/front/frontendDashboard/test/src/main.ts
@@ -1,9 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import 'localstorage-polyfill'
-
-global['localStorage'] = localStorage;
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
