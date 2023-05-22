3455d2299ff8c47d054c548f492fe76092dfb266
Jeanmaire dit cartier <yann.jeanmaire@gmail.com>
2023-05-22 11:27:31 +0200

    suppression d'une feature pour revenir a qq chose de propre

--- src/main/java/front/frontendDashboard/app/src/app/acceuil/acceuil.component.ts
+++ src/main/java/front/frontendDashboard/app/src/app/acceuil/acceuil.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import {animate, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-acceuil',
   templateUrl: './acceuil.component.html',
   styleUrls: ['./acceuil.component.css'],
-
+  animations: [
+    trigger('fade', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('500ms', style({ opacity: 1 }))
+      ]),
+      transition(':leave', [
+        animate('500ms', style({ opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class AcceuilComponent {
+  showSection1: boolean = true;
 
+  constructor() { }
+
+  ngOnInit() {}
 }
